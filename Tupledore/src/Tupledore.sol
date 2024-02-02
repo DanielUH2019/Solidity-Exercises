60c360891b43748bea56d1c479a4f22e83cb5341
DanielUH2019 <danielcardenascabrera2016@gmail.com>
2024-02-02 01:06:01 -0500

    Tuples

--- Tupledore/src/Tupledore.sol
+++ Tupledore/src/Tupledore.sol
@@ -12,4 +12,18 @@ contract Tupledore {
         4. Create a function called `returnTuple`, 
            that returns `userInfo` (as a tuple)
     */
+      struct UserInfo {
+         address userAddress;
+         uint256 userNumber;
+      }
+
+      UserInfo public userInfo;
+
+      function setTuple(address _userAddress, uint256 _userNumber) public {
+         userInfo = UserInfo(_userAddress, _userNumber);
+      }
+
+      function returnTuple() public view returns (address, uint256) {
+         return (userInfo.userAddress, userInfo.userNumber);
+      }
 }
